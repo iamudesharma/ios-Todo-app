c7cfcb054a3092251ae48313171f3a3a740de032
iamudesharma <udesh2568@gmail.com>
2022-08-18 21:19:38 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by udesh sharma on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
